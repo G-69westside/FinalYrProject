04684c43872c3878ea00ebef51ac0e1afcb33c0c
Tony Maluki <tonymaluki@gmail.com>
2022-05-25 17:26:07 +0300

    fixed minor footer bug

--- admin/Includes/templates/footer.php
+++ admin/Includes/templates/footer.php
@@ -42,13 +42,12 @@
 	</div>
 
 	<!-- INCLUDE JS SCRIPTS -->
-	<script src="Design/js/jquery.min.js">
-		< /scrip> <
-		script src = "https://unpkg.com/sweetalert/dist/sweetalert.min.js" >
-	</script>
+
+	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 	<script src="Design/js/bootstrap.bundle.min.js"></script>
 	<script src="Design/js/sb-admin-2.min.js"></script>
 	<script src="Design/js/main.js"></script>
+	<script src="Design/js/jquery.min.js"></script>
 
 	</body>
 
